78616f67e12d5e933098c46c111372187cdf6d7c
Przemek5901 <pbednarz10@gmail.com>
2024-11-09 22:59:31 +0100

    front-init

---
+++ Frontend/friend-nest/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {PrimeNGConfig} from 'primeng/api';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  title = 'friend-nest';
+
+  constructor(private primengConfig: PrimeNGConfig) {
+  }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+}
